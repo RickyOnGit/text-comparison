61b703fd4f40dcdcc2fbef4ebe0432edaf6f2677
Riccardo Castagna <3315954155@libero.it>
2018-06-28 08:55:22 +0200

    main php class

--- lib/class.comparetext.php
+++ lib/class.comparetext.php
@@ -9,21 +9,18 @@
 * example, to find the similar articles, in the cases of plagiarism, for       *
 * investigations of various kinds when it is necessary to analyze and compare  *
 * texts with each other.                                                       *
+*                                                                              *
+* In a text every sententence should end with a kind of end punctuaction.      *
+* The kinds of end puntuaction, AT THE END of a sentence are five and could    *
+* be: "." or ";" or "..." or "!" or "?", only these denotes the end of a       *
+* sentence.                                                                    *
+* So it need to develop something that find every end puntuaction, isolate     *
+* all sentences and than compare each other, all                               *
+* sentences, one at a time, contained in the text 1, with all sentences        *
+* contained in the text 2. Right ... ? Ok, good, let go on !                   *
 *******************************************************************************/
-
 class TextCompare{
 
-
-/*******************************************************************************
- * In a text every sententence should end with a kind of end punctuaction.     *
- * The kinds of end puntuaction, AT THE END of a sentence are five and could   *
- * be: "." or ";" or "..." or "!" or "?", only these denotes the end of a      *
- * sentence.                                                                   *
- * So it need to develop something that find every end puntuaction, isolate    *
- * all sentences and than compare each other, all                              *
- * sentences, one at a time, contained in the text 1 with all sentences        *
- * contained in the text 2. Right ... ? Ok, good, let go on !                  *
-*******************************************************************************/
 private function formatText($scrtxt, $symbol){
 $punctuation = array( '.' , ';' , '!' , '?','...' );
 $sym         = array($symbol,$symbol,$symbol,$symbol,$symbol);
@@ -59,6 +56,11 @@ return $n;
  * the next step will be to extract all sentences beetween the symbols         *
  * and put all the sentences into an array.                                    *
 *******************************************************************************/
+
+/*******************************************************************************
+ * function isolate find the end of every sentence where I have insert a       *
+ * $symbol and than build an array with all sentences isolated                 *   
+*******************************************************************************/
 private function isolate($srcTxt, $symbol, $numofsymbols){
 $strarray[0] = $srcTxt;  
 $y = 1;
@@ -89,7 +91,7 @@ return $strarray_1;
  * the function correlationText($text_1,$text_2), thank to the magic php       *
  * function similar_text : http://php.net/manual/en/function.similar-text.php  *
  * inside this function, that saved me to process some stat algorithm, return  *
- * the percentage of how much two test are similar with each other             *
+ * the percentage of how much two texts are similar with each other            *
 *******************************************************************************/
 
 public function correlationText($text_1,$text_2){
@@ -103,22 +105,21 @@ similar_text($text_1, $text_2, $percent);
 return $percent;
 }
 
+/*******************************************************************************
+ * cleanText is an optional function, remove specialchars and convert the text *
+ * into lower case. When you set this option to be TRUE the similitude         *
+ * percentange became a bit higher. The more homogeneous the two texts are,    *
+ * the better the analysis will be and, therefore, the result.                 *
+ * For better analysis is good to set this option to TRUE.                     *
+*******************************************************************************/
 public function cleanText($text){
 $string =  preg_replace('/[^a-z]+/i', ' ', mb_strtolower(preg_replace('/[^a-zA-Z0-9-_\.]/','_', $text), 'UTF-8'));
 return $string;
 }
+
 /*******************************************************************************
- * the function compareText($scrtxt_1, $scrtxt_2, $level, $specialchars=false) *
- * this function has four param:
- * 1) param 1 is a source text to compare
- * 2) param 2 is the sencond source text to compare
- * 3) param 3 is an integer from 0 to 100; if it is set to 0 it will return all 
- * sentences with all percentages of similarity. if it is set to 100 it will
- * return only the sentences that are exactly the same. A good set up could be 
- * around 60 and it will return all sentences whose similarity is greater than
- * 60 %.
- * 4) param 4 default is set to false, if it set to true it will convert the
- * two text in lowercase and will remove all special charset
+ * The function processText exec the functions and retun the array with all    *
+ * sentences ready to be compared                                              *
 *******************************************************************************/
 public function processText($scrtxt){
 $text = $this->formatText($scrtxt, 'x_#_z');
@@ -127,6 +128,7 @@ $txt = $this->strafter($text,'[#_start]');
 $arr = $this->isolate($txt, 'x_#_z', $num);
 return $arr; 
 }
+
 /*******************************************************************************
  * the function compareText($scrtxt_1, $scrtxt_2, $level, $specialchars=false) *
  * this function has four param:                                               *
